c5f1c174b44a7d1f6106b3b1457176ca19b420e6
Yash Chavan <yashcr0@gmail.com>
2023-10-03 23:12:00 +0530

    feat: added new file to predict fare price
    Signed-off-by: Yash Chavan <yashcr0@gmail.com>

--- fare_price_prediction.py
+++ fare_price_prediction.py
@@ -8,11 +8,11 @@ from sklearn.svm import SVR
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 
-df1 = pd.read_csv('/Users/YashChavan/Desktop/additional_work/Data/trip_data/trip_data_1.csv')
+df1 = pd.read_csv('trip_data_1.csv')
 df1.columns = df1.columns.str.replace(' ', '')
-df2 = pd.read_csv('/Users/YashChavan/Desktop/additional_work/Data/trip_data/trip_data_2.csv')
+df2 = pd.read_csv('trip_data_2.csv')
 df2.columns = df2.columns.str.replace(' ', '')
-df3 = pd.read_csv('/Users/YashChavan/Desktop/additional_work/Data/trip_data/trip_data_3.csv')
+df3 = pd.read_csv('trip_data_3.csv')
 df3.columns = df3.columns.str.replace(' ', '')
 
 df = pd.concat([df1, df2, df3],axis=0)
